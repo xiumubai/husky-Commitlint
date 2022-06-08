d3fdb081f9d03cc8b157b31239b32faa12a65b40
xiumubai <1547702880@qq.com>
2022-06-08 20:21:41 +0800

    feat: test

--- test.js
+++ test.js
@@ -2,7 +2,16 @@
  * @Author: 朽木白
  * @Date: 2022-06-08 13:59:07
  * @LastEditors: 1547702880@qq.com
- * @LastEditTime: 2022-06-08 15:58:35
- * @Description:
+ * @LastEditTime: 2022-06-08 20:19:08
+ * @Description: format
  */
 const test = 'test';
+const b = '3423424';
+
+const obj = {
+  name: 'janney',
+  age: 19,
+  hah: 54,
+};
+
+var abc = 1234;
